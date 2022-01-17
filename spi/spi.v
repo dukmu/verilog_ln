1f55d9c7fdad74cdc9429cca84772e1e6cd9be7f
dumu-biter <xflcount@163.com>
2022-01-17 11:23:17 +0800

    fix time schedule error

--- spi/spi.v
+++ spi/spi.v
@@ -12,10 +12,10 @@ module spi_module_master
 	input CLOCK, RESET, //positive_RESET, positive_CLOCK_50MHz
 	input I_rx_en, //read_enabled signal
 	input I_tx_en, //write_enabled signal
-	input [7:0] iData, //data to be output
-	output reg[7:0] oData, //data to be input
-	output reg iDone, //flag of transmit done
-	output reg oDone, //flag of receive done
+	output reg [7:0] iData, //master input
+	input [7:0] oData, //master output
+	output reg iDone, //master input
+	output reg oDone, //master output
 	
 	/*
 	IDLE = CPOL^CPHA
@@ -27,37 +27,43 @@ module spi_module_master
 	//standarded four-wire spi_bus
 	input I_spi_miso, //master input slave output
 	output reg O_spi_sck, //clock of spi
-	output reg O_spi_cs, //chip selected, positive_
+	output reg O_spi_cs, //chip selected, positive
 	output reg O_spi_mosi //master output slave input
 );
-
+reg delay;
 reg[2:0] r_rx_index;
 reg[2:0] r_tx_index;
 
 always@(posedge CLOCK or posedge RESET) begin
 	if(RESET) begin
-		oData<=8'd0;
+		iData<=8'd0;
 		iDone<=1'b0;
 		oDone<=1'b0;
-		O_spi_cs<=1'b1;
-		O_spi_sck<=CPOL^CPHA;
+		O_spi_cs<=1'b0; //is that correct?
+		O_spi_sck<=CPOL;
 		O_spi_mosi<=1'b0;
 		r_rx_index<=3'd7;
 		r_tx_index<=3'd7;
+		delay<=CPHA;
+		$display("RESET master");
 	end
 	else begin
 		if(I_rx_en&&(O_spi_sck==CPOL^CPHA)) begin
-			iDone<=1'b0;
-			oData[r_rx_index]<=I_spi_miso;
-			r_rx_index<=r_rx_index+3'd7;
-			if(r_rx_index==3'd0) begin
-				iDone<=1'b1;
+			if(delay) begin
+				iDone<=1'b0;
+				iData[r_rx_index]<=I_spi_miso;
+				r_rx_index<=r_rx_index+3'd7;
+				if(r_rx_index==3'd0) begin
+					iDone<=1'b1;
+				end
+			end
+			else begin
+				delay<=1'b1;
 			end
 		end
 		if(I_tx_en&&!(O_spi_sck==CPOL^CPHA)) begin
 			oDone<=1'b0;
-			O_spi_mosi<=iData[r_tx_index];
-			$display(r_tx_index);
+			O_spi_mosi<=oData[r_tx_index];
 			r_tx_index<=r_tx_index+3'd7;
 			if(r_tx_index==3'd0) begin
 				oDone<=1'b1;
@@ -82,10 +88,10 @@ module spi_module_slave
 	input RESET, //positive_RESET, positive_CLOCK_50MHz
 	input I_rx_en, //read_enabled signal
 	input I_tx_en, //write_enabled signal
-	input [7:0] iData, //data to be output
-	output reg[7:0] oData, //data to be input
-	output reg iDone, //flag of transmit done
-	output reg oDone, //flag of receive done
+	output reg [7:0] iData, //slave input
+	input [7:0] oData, //slave output
+	output reg iDone, //slave input
+	output reg oDone, //slave output
 	
 	/*
 	IDLE = CPOL^CPHA
@@ -100,32 +106,38 @@ module spi_module_slave
 	input I_spi_cs, //chip selected, positive_
 	input I_spi_mosi //master output slave input
 );
-
+reg delay;
 reg[2:0] r_rx_index;
 reg[2:0] r_tx_index;
 
 always@(posedge I_spi_sck or posedge RESET) begin
 	if(RESET) begin
-		oData<=8'd0;
+		iData<=8'd0;
 		iDone<=1'b0;
 		oDone<=1'b0;
 		O_spi_miso<=1'b0;
 		r_rx_index<=3'd7;
 		r_tx_index<=3'd7;
+		delay<=CPHA;
 	end
 	else begin
 	if(I_spi_cs==1'b1) begin
-		if(I_rx_en&&(CPOL^CPHA==1'b1)) begin
-			iDone<=1'b0;
-			oData[r_rx_index]<=I_spi_mosi;
-			r_rx_index<=r_rx_index+3'd7;
-			if(r_rx_index==3'd0) begin
-				iDone<=1'b1;
+		if(I_rx_en&&(CPOL^CPHA==1'b0)) begin
+			if(delay) begin
+				iDone<=1'b0;
+				iData[r_rx_index]<=I_spi_mosi;
+				r_rx_index<=r_rx_index+3'd7;
+				if(r_rx_index==3'd0) begin
+					iDone<=1'b1;
+				end
+			end
+			else begin
+				delay<=1'b1;
 			end
 		end
 		if(I_tx_en&&!(CPOL^CPHA==1'b0)) begin
 			oDone<=1'b0;
-			O_spi_miso<=iData[r_tx_index];
+			O_spi_miso<=oData[r_tx_index];
 			r_tx_index<=r_tx_index+3'd7;
 			if(r_tx_index==3'd0) begin
 				oDone<=1'b1;
@@ -136,26 +148,32 @@ always@(posedge I_spi_sck or posedge RESET) begin
 end
 always@(negedge I_spi_sck or posedge RESET) begin
 	if(RESET) begin
-		oData<=8'd0;
+		iData<=8'd0;
 		iDone<=1'b0;
 		oDone<=1'b0;
 		O_spi_miso<=1'b0;
 		r_rx_index<=3'd7;
 		r_tx_index<=3'd7;
+		delay<=CPHA;
 	end
 	else begin
 	if(I_spi_cs==1'b1) begin
-		if(I_rx_en&&(CPOL^CPHA==1'b0)) begin
-			iDone<=1'b0;
-			oData[r_rx_index]<=I_spi_mosi;
-			r_rx_index<=r_rx_index+3'd7;
-			if(r_rx_index==3'd0) begin
-				iDone<=1'b1;
+		if(I_rx_en&&(CPOL^CPHA==1'b1)) begin
+			if(delay) begin
+				iDone<=1'b0;
+				iData[r_rx_index]<=I_spi_mosi;
+				r_rx_index<=r_rx_index+3'd7;
+				if(r_rx_index==3'd0) begin
+					iDone<=1'b1;
+				end
+			end
+			else begin 
+				delay<=1'b1;
 			end
 		end
 		if(I_tx_en&&!(CPOL^CPHA==1'b1)) begin
 			oDone<=1'b0;
-			O_spi_miso<=iData[r_tx_index];
+			O_spi_miso<=oData[r_tx_index];
 			r_tx_index<=r_tx_index+3'd7;
 			if(r_tx_index==3'd0) begin
 				oDone<=1'b1;
